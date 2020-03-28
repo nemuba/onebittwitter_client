b99db05d5d774131a5ed253a2d4e33ae796176b9
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-03-28 09:39:13 -0300

    Get Trending topics via API

--- src/components/ProfilePage/index.js
+++ src/components/ProfilePage/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Row, Col } from 'react-materialize';
-import MainLayout from './../MainLayout'
-import UserInfoContainer from './../../containers/UserInfoContainer'
-import TweetListContainer from './../../containers/TweetListContainer'
-import TrendingTopicsContainer from './../../containers/TrendingTopicsContainer'
+import MainLayout from './../MainLayout';
+import UserInfoContainer from './../../containers/UserInfoContainer';
+import TweetListContainer from './../../containers/TweetListContainer';
+import TrendingTopicsContainer from './../../containers/TrendingTopicsContainer';
 
 const ProfilePage = () => (
   <MainLayout>
     <Row>
       <Col s={12} m={3} l={3}>
-      <UserInfoContainer/>
+        <UserInfoContainer/>
       </Col>
       <Col s={12} m={6} l={6}>
         <TweetListContainer/>
--- src/containers/TrendingTopicsContainer/index.js
+++ src/containers/TrendingTopicsContainer/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import TrendingTopics from '../../components/TrendingTopics'
+import TrendingTopics from '../../components/TrendingTopics';
+import axios from 'axios';
 
 class TrendingTopicsContainer extends Component {
   constructor(props) {
@@ -11,12 +12,14 @@ class TrendingTopicsContainer extends Component {
   }
 
 componentDidMount() {
-  this.setState({
-    hashtags: [
-      ['#OneBitCode', '10k'],
-      ['#RubyOnRails', '10k']
-    ]
+  axios.get(`${ process.env.REACT_APP_API}/trending`)
+  .then(response => {
+    this.setState(()=>{
+      console.log(response.data.hashtags);
+      return { hashtags: response.data.hashtags}
+    })
   })
+  .catch(error => console.log(`Erro: ${error}`));
 }
 
 render() {
