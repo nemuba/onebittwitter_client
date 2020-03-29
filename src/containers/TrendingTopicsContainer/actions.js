b315e1ea989df5a56208cf991401cb3ee19cc9a4
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-03-28 22:46:35 -0300

    Incluindo o Redux Thunk

--- src/containers/TrendingTopicsContainer/actions.js
+++ src/containers/TrendingTopicsContainer/actions.js
@@ -1,7 +1,17 @@
+import axios from 'axios';
+
 //* @action getHashtags
 //* @type 'UPDATE_TRENDINGS'
 export function getHashtags() {
-  return {
-    type: 'UPDATE_TRENDINGS'
-  }
+  const request = axios.get(`${process.env.REACT_APP_API}/trending`);
+
+  return (dispatch) =>{
+    request.then(resp => dispatch({type: 'UPDATE_TRENDINGS', payload: resp.data}),
+                  error => window.M.toast({
+                    html: 'Fetch Error Trendings Topics !',
+                    outDuration: 4000,
+                    classes: 'red rounded'
+                  })
+    );
+  };
 }
