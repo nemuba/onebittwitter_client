4c479c6b1211c39605cdf6e81e3be690f10f14e4
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-03-28 23:04:20 -0300

    Refatorando o Trending Topics

--- src/containers/TrendingTopicsContainer/actions.js
+++ src/containers/TrendingTopicsContainer/actions.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
+import { UPDATE_TRENDINGS } from './constants';
 
 //* @action getHashtags
-//* @type 'UPDATE_TRENDINGS'
+//* @type UPDATE_TRENDINGS
+//* @return action = {type: UPDATE_TRENDINGS, payload: data}
 export function getHashtags() {
   const request = axios.get(`${process.env.REACT_APP_API}/trending`);
 
   return (dispatch) =>{
-    request.then(resp => dispatch({type: 'UPDATE_TRENDINGS', payload: resp.data}),
+    request.then(resp => dispatch({type: UPDATE_TRENDINGS , payload: resp.data}),
                   error => window.M.toast({
                     html: 'Fetch Error Trendings Topics !',
                     outDuration: 4000,
